c5b0a0d37ba6fb8ac97508127a67278b7a73498d
Kevin Kroos <Kevin@145-24-200-4.wlan.hro.nl>
2016-02-18 13:44:31 +0100

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+TestAuth
+========
+
+A Symfony project created on February 14, 2016, 4:35 pm.
---
+++ app/config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "TestAuth"
+set :domain,      "developmentlocatie.nl"
+set :user,  "root"
+set :deploy_to,   "/home/admin/web/developmentlocatie.nl/public_html/"
+set :app_path,    "app"
+set :shared_files,      ["app/config/parameters.yml"]
+set :use_composer, true
+
+# Symfony console bin
+set :symfony_console,  "./bin/console"
+ # Symfony log path
+set :log_path, "var/logs"
+
+# Symfony cache path
+set :cache_path, "var/cache"
+
+# Symfony config file path
+set :app_config_path,   app_path + "/config"
+
+set   :scm,           :git
+set   :repository,    "file:///Users/Kevin/Documents/Websites/TestAuth"
+set   :deploy_via,    :copy
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+
+set :model_manager, "doctrine"
+# Or: `propel`
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set  :keep_releases,  3
+
+# Be more verbose by uncommenting the following line
+# logger.level = Logger::MAX_LEVEL
